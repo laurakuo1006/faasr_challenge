2a850d520a9f5c892ce856e20cccc2251a12fd03
Laura Kuo <ckuo28@wisc.edu>
2024-10-09 23:31:33 -0500

    Update server.R

--- server.R
+++ server.R
@@ -1,24 +1,21 @@
 # Load required libraries
-library(shiny)
-library(httr)
-library(aws.s3)
 
-minio_url <- "https://play.min.io"
+# minio_url <- "https://play.min.io"
 
 # Load MinIO credentials from 'faasr_env' file
-env_vars <- readLines("faasr_env")
-
-for (line in env_vars) {
-  without_quotes <- gsub('"', '', line)
-  key_value <- strsplit(without_quotes, "=")[[1]]
-  key <- key_value[1]
-  value <- key_value[2]
-  Sys.setenv(key = value)
-}
-
-Sys.setenv("AWS_ACCESS_KEY_ID" = Sys.getenv("My_Minio_Bucket_ACCESS_KEY"),
-           "AWS_SECRET_ACCESS_KEY" = Sys.getenv("My_Minio_Bucket_SECRET_KEY"),
-           "AWS_S3_ENDPOINT" = minio_url)
+# env_vars <- readLines("faasr_env")
+# 
+# for (line in env_vars) {
+#   without_quotes <- gsub('"', '', line)
+#   key_value <- strsplit(without_quotes, "=")[[1]]
+#   key <- key_value[1]
+#   value <- key_value[2]
+#   Sys.setenv(key = value)
+# }
+# 
+# Sys.setenv("AWS_ACCESS_KEY_ID" = Sys.getenv("My_Minio_Bucket_ACCESS_KEY"),
+#            "AWS_SECRET_ACCESS_KEY" = Sys.getenv("My_Minio_Bucket_SECRET_KEY"),
+#            "AWS_S3_ENDPOINT" = minio_url)
 
 
 # Define UI
@@ -57,7 +54,7 @@ server <- function(input, output) {
       #  url = minio_url
       #)
       
-      faasr_put_file(local_file="keyword.csv", remote_folder="laurafaasrchallenge", remote_file="keyword.csv")
+      faasr_put_file(local_file="keyword.csv", remote_folder="test", remote_file="keyword.csv")
       
       # (Here you would add code to trigger the FaaSr scraping step)
       # Example: Store the keyword in MinIO and trigger the workflow
